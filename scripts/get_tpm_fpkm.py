fda614356f1069d574df161a8bb85fc21562e9d4
Shriram Bhosle <sb43@sanger.ac.uk>
2019-07-15 17:13:48 +0100

    added working versions

--- scripts/get_tpm_fpkm.py
+++ scripts/get_tpm_fpkm.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import pandas as pd
+import numpy as np
 
 version = "1.0.0"
 
@@ -13,41 +14,72 @@ def prepare_data(**opt):
     count_file = opt['count_file']
     gene_len_file = opt['gene_len']
     index_label = 'ensid'
-    min_read_count = 1
+    min_read_count = opt['minimum_read_count']
+    gene_len_col=opt['gene_length_column']
+    user_biotypes=opt['transcript_biotype']
     # create data frame from htseq count data, ignore last 5 line of summary stats
-    count_df = create_df(count_file,5,[index_label,'count'], index_label)
+    count_df = create_df(count_file,5,0,[index_label,'count'], index_label)
     # create gene length data frame 
-    gene_len_df = create_df(gene_len_file, 0, [index_label, 'gene_name', 'length'], index_label)
+    gene_len_df = create_df(gene_len_file,0,1, [index_label,'gene','biotype','chr', 'mean','median', 'longest_isoform', 'merged'], index_label)
+
+    # check if gene length and count data frame have equal rows
+    ensid_comparison= np.array_equal(np.sort(count_df.index.values), np.sort(gene_len_df.index.values))
+    print("COUNT_ROWS:{} GENELEN_ROWS:{} ENSID_COMPARISON STATUS:{}".format(count_df.shape[0],gene_len_df.shape[0], ensid_comparison) )
+
+    if  not ensid_comparison:
+        sys.exit("Error: Ensembl ids in gene length and count file DO NOT match")
+
+    drop_columns=['mean','median', 'longest_isoform', 'merged']
+    drop_columns.remove(gene_len_col)
+    # drop other columns except considered for gene length ...
+    gene_len_df.drop(drop_columns, axis=1, inplace=True)
 
     combined_df=pd.concat([gene_len_df,count_df], axis=1, sort =True)
-    # filter genes with 0 read count
-    combined_df.drop(combined_df[combined_df['count'] < min_read_count].index, inplace=True)
+    # filter genes with user defined read counts read count, these will be set to 0
+    combined_df['unfiltered_count'] = combined_df['count']
+    combined_df.loc[combined_df['count'] < min_read_count, 'count'] = 0
     #https: // docs.gdc.cancer.gov/Data/Bioinformatics_Pipelines/Expression_mRNA_Pipeline/  # fpkm
-    combined_df['fpkm']= (combined_df['count'] * 1e9) / (combined_df['count'].sum() * combined_df['length'])
+    combined_df['fpkm']= (combined_df['count'] * 1e9) / (combined_df['count'].sum() * combined_df[gene_len_col])
     # FPKM_UQ https://docs.gdc.cancer.gov/Data/Bioinformatics_Pipelines/Expression_mRNA_Pipeline/#upper-quartile-fpkm
-    uq=combined_df['count'].quantile(q=0.75, interpolation='nearest')
-
-    combined_df['fpkm_uq'] = (combined_df['count'] * 1e9) / (uq * combined_df['length'])
+    # filter data frame for protein coding genes only...
+    fpkm_quantile_df=combined_df[combined_df.biotype.isin(user_biotypes)]
+    # calculate upper quantile value...
+    uq=fpkm_quantile_df['count'].quantile(q=0.75, interpolation='nearest')
+    combined_df['fpkm_uq'] = (combined_df['count'] * 1e9) / (uq * combined_df[gene_len_col])
     #TPM = FPKM / (sum of FPKM over all genes/transcripts) * 10^6
     combined_df['tpm'] = combined_df['fpkm']/ combined_df['fpkm'].sum() * 1e6
-	# print results
-    _print_df(combined_df, index_label, count_file)
+	# print results and header lines ....
+
+    print(combined_df.shape)
+
+    header_data={}
+    for key in opt:
+        if key is not None:
+            header_data["##"+key+"="]=str(opt[key])
+
+    header_data['##Upper_Quartile_Val=']=str(uq)
+    header_data['##WARNING user filtered read count is set to ='] = 0
+
+    header_df=pd.DataFrame.from_dict(header_data,orient='index')
+
+    _print_df(combined_df, index_label, count_file, header_df)
 
     return
 
 # print data frame 
-def _print_df(mydf, index_label, count_file ):
-    mydf = mydf.round(decimals=8)
+def _print_df(mydf, index_label, count_file, header_df ):
+    mydf = mydf.round(decimals=2)
     (_, name) = os.path.split(count_file)
     (out_file, _) = os.path.splitext(name)
-    mydf.to_csv(out_file + '_count_fpkm_tpm.tsv', sep='\t', mode='w', header=True, index=True, index_label=index_label, doublequote=False)
+    header_df.to_csv(out_file + '_count_fpkm_tpm.tsv',sep="\t", header=False, mode ='w')
+    mydf.to_csv(out_file + '_count_fpkm_tpm.tsv', sep='\t', mode='a', header=True, index=True, index_label="#"+index_label, doublequote=False)
 
-def create_df(infile,skip_footer,col_names, index_label):
-    df = pd.read_csv(infile, compression='infer', sep="\t", skipfooter=skip_footer, engine='python', names=col_names,  header=None, index_col=index_label)
+def create_df(infile,skip_footer,skip_header,col_names, index_label):
+    df = pd.read_csv(infile, compression='infer', sep="\t", skipfooter=skip_footer, skiprows=skip_header, engine='python', names=col_names,  header=None, index_col=index_label)
     return df
 
 def main():
-    usage = "\n %prog [options] -c count_file.tsv -g gene_len.tsv  -a archive.tar.gz"
+    usage = "\n %prog [options] -c count_file.tsv -g gene_len.tsv "
 
     optParser = argparse.ArgumentParser(prog='get_fpkm')
     optional = optParser._action_groups.pop()
@@ -57,7 +89,16 @@ def main():
                           default=None, help="count file path, format: gene count (last five lines of summary were ignored)")
 
     required.add_argument("-g", "--gene_len", type=str, dest="gene_len", required=True,
-                          default=None, help="gene length file path, format: ensid gene_name length")
+                          default=None, help="gene length file path, format: ensid gene_name length [Warning first line will be skipped as header]")
+
+    optional.add_argument("-minrc", "--minimum_read_count", type=int, dest="minimum_read_count", required=False,
+                          default=0, help="Minimum read count to consider for fpkm,tpm calculations default:0")
+
+    optional.add_argument("-len_col", "--gene_length_column", type=str, dest="gene_length_column", required=False,
+                          default="longest_isoform", help="gene length column name to use from gene_len file default:longest_isoform")
+
+    optional.add_argument("-biotype", "--transcript_biotype", type=str, nargs='+', dest="transcript_biotype", required=False,
+                          default=["protein_coding"], help="transcript biotypes to choose for fpkm_uq (upper quartile normalization), default: protein_coding,")
 
     optional.add_argument("-v", "--version", action='version', version='%(prog)s ' + version)
     optional.add_argument("-q", "--quiet", action="store_false", dest="verbose", required=False, default=True)
@@ -68,11 +109,11 @@ def main():
         sys.exit(1)
     opts = optParser.parse_args()
     if not(opts.gene_len or opts.count_file):
-        sys.exit('\nERROR Arguments required\n\tPlease run: get_fpkm.py --help\n')
+        sys.exit('\nERROR Arguments required\n\tPlease run: get_tpm_fpkm.py --help\n')
 
     # vars function returns __dict__ of Namespace instance
     else:
-        print("Converting count to FPKM....")
+        print("Converting count to FPKM, TPM and FPKM_UQ....")
         prepare_data(**vars(opts))
 
 if __name__ == "__main__":
---
+++ scripts/merge_samples.py
@@ -0,0 +1,107 @@
+import os
+import sys
+import argparse
+import pandas as pd
+
+version = "1.0.0"
+
+# converts count data to fpkm and tpm values
+# needs some mofifications before running ....
+# prepare data 
+# print data frame
+def _print_df(mydf, outfile, index_label):
+    mydf.to_csv(outfile, sep='\t', mode='w', header=True, index=True, index_label=index_label, doublequote=False)
+
+def create_df(infile,skip_footer,skip_header,col_names, index_label):
+    df = pd.read_csv(infile, compression='infer', sep="\t", skipfooter=skip_footer, skiprows=skip_header, engine='python', names=col_names,  header=None, index_col=index_label)
+    return df
+
+
+# merge per sample files to a single file
+
+def merge_files(exp_type, **opt):
+    out_file = opt.get('outfile',None)
+    gene_len_file = opt['gene_len'].strip()
+    merge_ext=opt['merge_ext'].strip()
+    gene_len_col = opt['gene_length_column'].strip()
+
+    mydfs=[]
+    column_order=[]
+    index_label = 'ensid'
+    gene_len_df = create_df(gene_len_file,0,1, [index_label,'gene','biotype','chr', 'mean','median', 'longest_isoform', 'merged'], index_label)
+    # drop unecessary columns....
+    drop_columns = ['mean', 'median', 'longest_isoform', 'merged']
+    drop_columns.remove(gene_len_col)
+    # drop other columns except considered for gene length ...
+    gene_len_df.drop(drop_columns, axis=1, inplace=True)
+
+    mydfs.append(gene_len_df)
+    for (dirpath, dirnames, filenames) in os.walk("."):
+        for myfile in filenames:
+            if myfile.endswith(merge_ext):
+                (sample,_)=os.path.splitext(myfile)
+                sample=sample.split(".")[0]
+                full_path=os.path.join(dirpath, myfile)
+                tmpdf=create_df_to_merge(full_path,8)
+                # drop column required to be merged
+                tmpdf=tmpdf[[exp_type]]
+                #tmpdf.drop(['gene_name','length','tpm','count'], axis=1, inplace=True, errors='raise' )
+                tmpdf.columns=[sample]
+                column_order.append(sample)
+                mydfs.append(tmpdf)
+        combined_df = pd.concat(mydfs, axis=1, sort=True, verify_integrity=True)
+        combined_df.fillna(0,inplace=True)
+        #column_order.sort()
+        #column_order.insert(0,'gene')
+        #combined_df=combined_df[column_order]
+
+        _print_df(combined_df, 'merged_' + exp_type + '.tsv', index_label)
+    return
+
+def create_df_to_merge(infile,skip_header):
+    df = pd.read_csv(infile, compression='infer', skiprows=skip_header, sep="\t", index_col=['#ensid'])
+    return df
+
+
+def main():
+    usage = "\n %prog [options] -g gene_len.tsv  -merge_ext _count_fpkm_tpm.tsv"
+
+    optParser = argparse.ArgumentParser(prog='get_fpkm')
+    optional = optParser._action_groups.pop()
+    required = optParser.add_argument_group('required arguments')
+
+    required.add_argument("-g", "--gene_len", type=str, dest="gene_len", required=True,
+                          default=None, help="gene length file path, format: ensid gene_name length")
+
+    optional.add_argument("-len_col", "--gene_length_column", type=str, dest="gene_length_column", required=False,
+                          default="longest_isoform",
+                          help="gene length column name to use from gene_len file default:longest_isoform")
+
+    required.add_argument("-merge_ext", "--merge_ext", type=str, dest="merge_ext",
+                          default=None, help="merged results files unique extension")
+
+    optional.add_argument("-col_name", "--column_names", type=str, nargs='+', dest="column_names",
+                          required=False,
+                          default=["unfiltered_count","count","tpm","fpkm","fpkm_uq"],
+                          help=" column names to merger defaut: count tpm fpkm fpkm_uq")
+
+    optional.add_argument("-v", "--version", action='version', version='%(prog)s ' + version)
+    optional.add_argument("-q", "--quiet", action="store_false", dest="verbose", required=False, default=True)
+
+    optParser._action_groups.append(optional)
+    if len(sys.argv) == 1:
+        optParser.print_help()
+        sys.exit(1)
+    opts = optParser.parse_args()
+    if not (opts.gene_len):
+        sys.exit('\nERROR Arguments required\n\tPlease run: merge_samples.py --help\n')
+    # vars function returns __dict__ of Namespace instance
+    # use this step after analysis to merge files into single tsv file
+    opt= vars(opts)
+    print(opt)
+    for type in opt['column_names']:
+       print("Merging val {} for samples into a single file....: ".format(type))
+       merge_files(type, **vars(opts))
+
+if __name__ == "__main__":
+    main()
