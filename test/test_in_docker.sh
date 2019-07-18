74e47fbc80200c6abdab9676cdedc86caa509082
Yaobo Xu <yx2@sanger.ac.uk>
2019-07-18 15:13:23 +0000

    CI tests run without creating a new folder

--- test/test_in_docker.sh
+++ test/test_in_docker.sh
@@ -2,16 +2,14 @@
 
 set -xe
 
-mkdir test && cd test
+gtftools.py -l test.len Data/test.gtf && diff -q test.len Data/test.len
 
-gtftools.py -l test.len ../Data/test.gtf && diff test.len ../Data/test.len
-
-get_tpm_fpkm.py -c ../Data/test.count -g test.len && diff -q <(grep -v '#' test_count_fpkm_tpm.tsv) <(grep -v '#' ../Data/test_count_fpkm_tpm.tsv)
-get_tpm_fpkm.py -c ../Data/test2.count -g test.len && diff -q <(grep -v '#' test2_count_fpkm_tpm.tsv) <(grep -v '#' ../Data/test2_count_fpkm_tpm.tsv)
+get_tpm_fpkm.py -c Data/test.count -g test.len && diff -q <(grep -v '#' test_count_fpkm_tpm.tsv) <(grep -v '#' Data/test_count_fpkm_tpm.tsv)
+get_tpm_fpkm.py -c Data/test2.count -g test.len && diff -q <(grep -v '#' test2_count_fpkm_tpm.tsv) <(grep -v '#' Data/test2_count_fpkm_tpm.tsv)
 
 merge_samples.py -g test.len -merge_ext _count_fpkm_tpm.tsv
-diff -q merged_count.tsv ../Data/merged_count.tsv
-diff -q merged_fpkm.tsv ../Data/merged_fpkm.tsv
-diff -q merged_fpkm_uq.tsv ../Data/merged_fpkm_uq.tsv
-diff -q merged_tpm.tsv ../Data/merged_tpm.tsv
-diff -q merged_unfiltered_count.tsv ../Data/merged_unfiltered_count.tsv
+diff -q merged_count.tsv Data/merged_count.tsv
+diff -q merged_fpkm.tsv Data/merged_fpkm.tsv
+diff -q merged_fpkm_uq.tsv Data/merged_fpkm_uq.tsv
+diff -q merged_tpm.tsv Data/merged_tpm.tsv
+diff -q merged_unfiltered_count.tsv Data/merged_unfiltered_count.tsv
